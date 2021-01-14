7347479712dc65f9425234b049790834390b40c4
Macro303 <Macro303@users.noreply.github.com>
2021-01-14 17:08:55 +1300

    Refine Config setup

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -8,26 +8,23 @@ LOGGER = logging.getLogger(__name__)
 
 
 def save_config():
-    validate_config()
     with open(config_file, 'w', encoding='UTF-8') as yaml_file:
         yaml.safe_dump(CONFIG, yaml_file)
-
-def validate_config():
-    if 'Prefix' not in CONFIG:
-        CONFIG['Prefix'] = '!'
-    if 'Token' not in CONFIG:
-        CONFIG['Token'] = None
-    if 'Moderator' not in CONFIG:
-        CONFIG['Moderator'] = []
-    if 'Blacklist' not in CONFIG:
-        CONFIG['Blacklist'] = []
 
 
 config_file = TOP_DIR.joinpath('config.yaml')
 if config_file.exists():
     with open(config_file, 'r', encoding='UTF-8') as yaml_file:
-        CONFIG = yaml.safe_load(yaml_file)
+        CONFIG = yaml.safe_load(yaml_file) or {
+            'Prefix': '!',
+            'Token': None,
+            'Blacklist': []
+        }
 else:
     config_file.touch()
-    CONFIG = {}
+    CONFIG = {
+        'Prefix': '!',
+        'Token': None,
+        'Blacklist': []
+    }
 save_config()
